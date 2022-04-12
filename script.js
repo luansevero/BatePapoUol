d5be410d158284df08c0743dd27abf9a4c1d41fb
LuanSevero <luansevero@icloud.com>
2022-04-12 14:46:55 -0300

    MainProject(Bônus2) > Tela de Entrada

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function login(element){
+    let nametyped = document.querySelector(`.write-name`)
+    if(verifyName((nametyped.value))){
+        element.parentNode.classList.remove(`active`)
+    }
+}
+function verifyName(nametyped){
+    if(nametyped == 0){
+        return false
+    } else {
+        return true
+    }
+}
 function menu(){
     document.querySelector(`.mainsidebar`).classList.add(`active`)
 }
