da0593f586401ec35657153b14fd66db2c976073
LuanSevero <luansevero@icloud.com>
2022-04-12 16:36:30 -0300

    MainProject(Bônus : Participantes Ativos) > Troca do check, e troca do parâmetro

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+function localTime(){
+    let time = new Date()
+    let hour = time.getHours()
+    let min = time.getMinutes()
+    let sec = time.getSeconds()
+    let clock = `${("0" + hour).slice(-2)}:${("0" + min).slice(-2)}:${("0" + sec).slice(-2)}`
+    return clock
+}
+//
+let users;
+let visibility;
+function msgFor(element){
+    const msgto = document.querySelector(`.sendTo`)
+    const clickArea = element.classList[0]
+    const allAreas = document.querySelectorAll(`.${clickArea}`)
+    for(let i = 0; i < allAreas.length; i++){
+        if(allAreas[i].classList.contains(`selected`)){
+            allAreas[i].classList.remove(`selected`)
+            allAreas[i].querySelector(`.checkmark`).classList.remove(`selected`)
+        }
+    }
+    element.classList.add(`selected`)
+    element.querySelector(`ion-icon.checkmark`).classList.add(`selected`)
+    if(clickArea == "users"){
+        users =  element.querySelector(`p`).innerHTML
+        msgto.querySelectorAll(`span`)[0].innerHTML = users
+    } else if(clickArea == "visibilityType"){
+        visibility = element.querySelector(`p`).innerHTML
+        msgto.querySelectorAll(`span`)[1].innerHTML = `(${visibility})`
+        
+    }
+
+}
+
+
+
+
 function login(element){
     let nametyped = document.querySelector(`.write-name`)
     if(verifyName((nametyped.value))){
@@ -21,11 +58,16 @@ function closeMenu(element){
         }
     }
 }
-function localTime(){
-    let time = new Date()
-    let hour = time.getHours()
-    let min = time.getMinutes()
-    let sec = time.getSeconds()
-    let clock = `${("0" + hour).slice(-2)}:${("0" + min).slice(-2)}:${("0" + sec).slice(-2)}`
-    return clock
+
+function sendMessage(){
+    let localtime = localTime()
+    let msgText = document.querySelector(`.write-msg`)
+    let newchatMsg = document.createElement(`div`)
+    newchatMsg.classList.add(`mensagens`)
+    newchatMsg.classList.add(`${msgTypeClass}`)
+    newchatMsg.innerHTML = `
+    <div class="msg">
+        <p><time>${localtime}</time>  <strong>${user}</strong> ${msgType}:  ${msgText.value}</p>
+    </div>
+    `
 }
