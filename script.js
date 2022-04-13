209c5bbad4f143c1fa4064766b1b90c21221c428
LuanSevero <luansevero@icloud.com>
2022-04-13 15:44:07 -0300

    MainProject > Rolagem do chat automática, criação do chat, e criação da sidebar, e load para entrar

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 //Entrar na Sala API
-function login(){
+function login(element){
     const nametyped = document.querySelector(`.write-name`).value
     const username = { name: nametyped}
+    element.parentNode.classList.toggle(`active`)
+    document.querySelector(`.loading`).classList.toggle(`active`)
     const promess = axios.post(`https://mock-api.driven.com.br/api/v6/uol/participants `, username);
     promess.then(nomecadastrado)
     promess.catch(nomecomFalha)
-
 }
+participantsList()
 function nomecadastrado(){
     const username2 = document.querySelector(`.write-name`).value
     document.querySelector(`.entrance`).classList.remove(`active`);
-    const idInterval = setInterval(keepConection, 4000, username2)
-    const id2Interval = setInterval(participantsList, 10000)
+    setInterval(keepConection, 4000, username2)
+    setInterval(participantsList, 10000,)
+    setInterval(searchMenssages, 3000)
     searchMenssages()
 }
 function nomecomFalha(){
     alert(`Nome já foi cadastrado, tente um novo nome!`)
+    window.location.reload()
 }
 //Manter Conexão API
 function keepConection(userN){
@@ -23,26 +27,26 @@ function keepConection(userN){
         name: userN
     }
     const conectionpromess = axios.post(`https://mock-api.driven.com.br/api/v6/uol/status`, dados)
-
 }
 
 
 //Lista de Participantes API
 function participantsList(){
-    const listPromess = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    const listPromess = axios("https://mock-api.driven.com.br/api/v6/uol/participants");
     listPromess.then(createList)
 }
 function createList(list){
-    let userlogin = document.querySelector(`.write-name`).value
-    for(let i = 0; i < list.length; i++){
-        if(list.data.name[i] != userlogin){
+    document.querySelector(`.participantslist`).innerHTML = ""
+    let usernametyped = document.querySelector(`.write-name`).value
+    for(let i = 0; i < list.data.length; i++){
+        if(usernametyped != list.data[i].name){
         let newParticipant = document.createElement(`div`)
-        newParticipant.classList.add(`user`)
+        newParticipant.classList.add(`users`)
         newParticipant.setAttribute(`onclick`, `msgFor(this)`)
         newParticipant.innerHTML = `
         <div class="users-container">
             <ion-icon class="iconPerson" name="person-circle"></ion-icon>
-            <p class="otherUsers">${list.data.name[i]}</p>
+            <p class="otherUsers">${list.data[i].name}</p>
         </div>
         <ion-icon class="checkmark" name="checkmark-sharp"></ion-icon>
         `  
@@ -84,15 +88,6 @@ function msgFor(element){
     }
 
 }
-
-function localTime(){ //Pega a data local
-    let time = new Date()
-    let hour = time.getHours()
-    let min = time.getMinutes()
-    let sec = time.getSeconds()
-    let clock = `${("0" + hour).slice(-2)}:${("0" + min).slice(-2)}:${("0" + sec).slice(-2)}`
-    return clock
-}
 //Função de Criação de mensagem(Feito)
 function sendMessage(){
     const from = document.querySelector(`.write-name`).value //De quem
@@ -108,7 +103,7 @@ function sendMessage(){
         from: from,
         to: to,
         text: text,
-        type: tipo
+        type: tipo,
     }
     const sendmsgPromess = axios.post(`https://mock-api.driven.com.br/api/v6/uol/messages`, mensage);
     sendmsgPromess.then(mensagemCadastrada)
@@ -119,25 +114,35 @@ function mensagemCadastrada(){
     searchMenssages()
 }
 function mensagemComFalha(err){
-    alert(err.responde.status)
     window.location.reload()
 }
 //Função de Procura de Mensagens
 function searchMenssages(){
-    const searchmsgPromess = axios.get(`http://mock-api.driven.com.br/api/v6/uol/messages`)
+    const searchmsgPromess = axios.get(`https://mock-api.driven.com.br/api/v6/uol/messages`)
     searchmsgPromess.then(backUpMsg)
 }
 function backUpMsg(allmsg){
-    document.querySelector(`.chat`).innerHTML = ""
-    for(let i = 0; i < allmsg.length; i++){
+    const elementoQueQueroQueApareca = document.querySelector('.chatmargins');
+    document.querySelector(`main.chat`).innerHTML = ""
+    for(let i = 0; i < allmsg.data.length; i++){
         let newchatMsg = document.createElement(`div`)
         newchatMsg.classList.add(`mensagens`)
-        newchatMsg.classList.add(`${allmsg.data.type[i]}`)
+        newchatMsg.classList.add(`${allmsg.data[i].type}`)
+        if(allmsg.data[i].type == "private_message"){
         newchatMsg.innerHTML = `
         <div class="msg">
-            <p><time>(${allmsg.data.time[i]})</time> <strong>${allmsg.data.from[i]}</strong> para ${allmsg.data.to[i]}: ${allmsg.data.text[i]}</p>
+            <p><time>(${allmsg.data[i].time})</time> <strong>${(allmsg.data[i].from)}</strong> reservadamente para <strong>${allmsg.data[i].to}</strong>: ${allmsg.data[i].text}</p>
         </div>
         `
         document.querySelector(`.chat`).appendChild(newchatMsg)
+        } else {
+            newchatMsg.innerHTML = `
+            <div class="msg">
+                <p><time>(${allmsg.data[i].time})</time> <strong>${(allmsg.data[i].from)}</strong> para ${allmsg.data[i].to}: ${allmsg.data[i].text}</p>
+            </div>
+            `
+            document.querySelector(`.chat`).appendChild(newchatMsg)
+        }
     }
+    elementoQueQueroQueApareca.scrollIntoView(false);
 }
